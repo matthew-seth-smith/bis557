aabbb54163bb36030cdd0f8364505ff84ad30395
Master <matthew.s.smith@yale.edu>
2018-11-06 12:45:17 -0500

    Cleaned up the function and vignette code.

--- R/break-kkt.R
+++ R/break-kkt.R
@@ -2,33 +2,31 @@
 #' 
 #' @description This function returns a logical vector of the same length as the coefficient vector that says where the Karush-Kuhn-Tucker (KKT) Conditions are broken.
 #' @param b the coefficient vector to check
-#' @param X_stand the data matrix for the LASSO regression, assumed to be standardized (mean 0 and variance 1 for each column) prior
-#' @param y the vector of responses
+#' @param X the data matrix for the LASSO regression, assumed to be standardized (mean 0 and variance 1 for each column) prior. We also assume there is no intercept column
+#' @param y the vector of responses, which we assume has also had the mean subtracted
 #' @param lambda the LASSO penalty constant (we assume there is no ridge regression component in this elastic net)
 #' @return A logical vector of the same length as `b` that says whether each coefficient breaks the KKT Conditions
 #' @import stats
 #' @import MASS
 #' @examples
-#' X <- model.matrix(Sepal.Length ~ ., iris)
-#' plot(cv.glmnet(X, iris$Sepal.Length, standardize=TRUE)) #Get an estimate for the optimal lambda
-#' lambda <- exp(-4.5) #Based on the above plot, after exponentiating
-#' b <- glmnet(X, iris$Sepal.Length, lambda=lambda, standardize=TRUE)$beta
-#' X_stand <- scale(X) #Standardize the data matrix X before plugging it into this function
-#' break_kkt(b, X_stand, iris$Sepal.Length, lambda)
+#' X <- scale(model.matrix(Sepal.Length ~ . -1, iris))
+#' y <- iris$Sepal.Length - mean(iris$Sepal.Length)
+#' plot(cv.glmnet(X, y, standardize=FALSE, intercept=FALSE)) #Get an estimate for the optimal lambda
+#' We will use the largest lambda value that is within one standard deviation of the lambda that gives the minimum MSE
+#' lambda <- fit$lambda.1se
+#' b <- fit$glmnet.fit$beta[,fit$lambda == lambda]
+#' break_kkt(b, X, y, lambda)
 #' @export
-break_kkt <- function(b, X_stand, y, lambda){
-  if(length(b) != ncol(X_stand) | length(y) != nrow(X_stand)){ #First check to make sure the dimensions of everything agree
+break_kkt <- function(b, X, y, lambda){
+  if(length(b) != ncol(X) | length(y) != nrow(X)){ #First check to make sure the dimensions of everything agree
     print("Error: incorrect number of dimensions somewhere.")
   }else{
     ret <- rep(FALSE, length(b)) #Initialize the return object to be a vector of FALSE
-    resids <- y - X_stand %*% b
-    mys <- apply(X_stand, 2, function(xj) crossprod(xj, resids)) / lambda / nrow(X_stand)
-    # May need to round this
-    #browser()
     for(j in 1:length(ret)){ #For each coefficient
-#      browser()
-      test_var <- sum(X_stand[,j] * (y - X_stand %*% b)) /nrow(X_stand) #Using EQ 7.37 on page 188, using element-wise multiplication before we sum over the rows
-      s <- test_var / lambda #This is the s-value in the equation to test the KKT Conditions
+      test_var <- sum(X[,j] * (y - X %*% b)) /nrow(X) #Using EQ 7.37 on page 188, using element-wise multiplication before we sum over the rows
+      s <- round(test_var / lambda) #This is the s-value in the equation to test the KKT Conditions
+      # We unfortunately have to round this to the nearest integer because glmnet does not produce very accurate coefficients,
+      # even when we provide it a vector of lambda values
       if(b[j] > 0){
         ret[j] <- !(s == 1) #We are testing to see if the KKT Conditions are BROKEN, not satisfied
       }else if(b[j] == 0){
--- inst/doc/Homework_3_vignette.R
+++ inst/doc/Homework_3_vignette.R
@@ -3,25 +3,24 @@ library(bis557)
 library(glmnet) #For the glmnet function
 
 ## ------------------------------------------------------------------------
-devtools::document()
 break_kkt
 
 ## ------------------------------------------------------------------------
-X <- model.matrix(Sepal.Length ~ . -1, iris)
-X <- scale(X)
-iris$Sepal.Length <- iris$Sepal.Length - mean(iris$Sepal.Length)
-fit <- cv.glmnet(X, iris$Sepal.Length, standardize=FALSE, intercept = FALSE)
+X <- scale(model.matrix(Sepal.Length ~ . -1, iris))
+y <- iris$Sepal.Length - mean(iris$Sepal.Length)
+fit <- cv.glmnet(X, y, standardize=FALSE, intercept = FALSE)
 plot(fit) #Get an estimate for the optimal lambda
-lambda <- fit$lambda.min #Based on the above plot, after exponentiating
-b <- fit$glmnet.fit$beta[,fit$lambda == fit$lambda.min]
-#  glmnet(X, iris$Sepal.Length, lambda=lambda, standardize=TRUE)$beta
-#b <- as.matrix(b)
-#X_stand <- scale(X) #Standardize the data matrix X before plugging it into this function
-document()
-break_kkt(b, X, iris$Sepal.Length, lambda)
+# We will use the largest lambda value that is within one standard deviation of the lambda that gives the minimum MSE
+lambda <- fit$lambda.1se
+b <- fit$glmnet.fit$beta[,fit$lambda == lambda]
+b
+break_kkt(b, X, y, lambda)
 
-## ---- eval=FALSE---------------------------------------------------------
-#  b
-#  b$Petal.Length <- 0 #This was originally non-zero
-#  break_kkt(b_updated, X_stand, iris$Sepal.Length, lambda)
+## ------------------------------------------------------------------------
+b_updated <- b
+b_updated["Petal.Length"] <- 0 #This was originally non-zero
+break_kkt(b_updated, X, y, lambda)
+b_updated2 <- b
+b_updated2["Speciesversicolor"] <- 0.01 #This was originally zero
+break_kkt(b_updated2, X, y, lambda)
 
