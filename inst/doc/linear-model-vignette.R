14da9a9bfb68bde02187ab5403ae32a4589248f3
r-medicine <r-medicine@protonmail.com>
2018-12-11 12:54:15 -0500

    Fixes to %*%.

--- inst/doc/linear-model-vignette.R
+++
@@ -1,5 +0,0 @@
-## ------------------------------------------------------------------------
-library(bis557)
-fit <- linear_model(Sepal.Length ~ ., iris)
-# summary(fit) Comment this out until know how to fill in the rest of the lm object
-
